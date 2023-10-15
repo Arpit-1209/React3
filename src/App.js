01cff6482d1c403b1ea61cc6f51d65b8f01aa82a
Arpit Srivastava <112778916+Arpit-1209@users.noreply.github.com>
2023-10-15 12:52:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import ColorPicker from './components/ColorPicker';
-import "C:/Users/Arpit Srivastava/OneDrive/Desktop/MERN fullstack/node_modules/bootstrap/dist/css/bootstrap.min.css";
+//import "C:/Users/Arpit Srivastava/OneDrive/Desktop/MERN fullstack/node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const colors = ["red", "blue", "green", "yellow"];
   return (
     <div className="App">
       <ColorPicker colors={colors}/>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" ></link>    
     </div>
   );
 }
